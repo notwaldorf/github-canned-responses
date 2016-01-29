7e1f61fb89e1e5ce0515a9d0e744bca44754ea6f
Monica Dinculescu <noms@google.com>
2016-01-29 10:24:59 -0800

    Backgroundscripts Rule Everything Around Me

---
+++ background.js
@@ -0,0 +1,61 @@
+var defaultAnswers = [
+  {
+    name: 'Issue: thanks! help fix?',
+    description: "Thanks a lot for filing this issue! Would you like to write a patch for this? We'd be more than happy to walk you through the steps involved."
+  },
+  {
+    name: 'Issue: thanks! looking!',
+    description: "Thanks a lot for filing this issue! We'll triage and take a look at it as soon as possible!"
+  },
+  {
+    name: 'Issue: looks inactive',
+    description: "This issue is fairly old and there hasn't been much activity on it. Closing, but please re-open if it still occurs."
+  },
+  {
+    name: 'Issue: closing, no repro steps',
+    description: "This issue has no reproducible steps. Please re-open this issue if it still occurs, with a JSBin containing a set of reproducible steps. Check this element's CONTRIBUTING.md for an example."
+  },
+  {
+    name: 'Issue: provide repro steps',
+    description: "Please provide a JSBin containing a set of reproducible steps. Check this element's CONTRIBUTING.md for an example."
+  },
+  {
+    name: 'Issue: cannot reproduce',
+    description: "Not reproducible in the latest release. Please re-open this issue if it still occurs, with a JSBin containing a set of reproducible steps. Check this element's CONTRIBUTING.md for an example."
+  },
+  {
+    name: 'PR: thanks! looking!',
+    description: "Thanks for your contribution! We'll triage and take a look at it as soon as possible!"
+  },
+  {
+    name: 'PR: against MD spec',
+    description: "Thanks for your thoughtful contribution! Unfortunately, this is against the Material Design spec, and we can't accept it at this time."
+  },
+  {
+    name: 'PR: needs test',
+    description: "Please add a test case that tests the problem this PR is fixing"
+  }
+];
+
+function getAnswersListFromStorage() {
+  // Load the answers.
+  var localStorageKey = '__GH_CANNED_ANSWERS__EXT__';
+  var saved = localStorage.getItem(localStorageKey);
+  var answers;
+
+  if (!saved || saved === '') {
+    localStorage.setItem(localStorageKey, JSON.stringify(defaultAnswers));
+    answers = defaultAnswers;
+  } else {
+   answers = JSON.parse(localStorage.getItem(localStorageKey));
+  }
+
+  return answers;
+}
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message === 'load') {
+    var answers = getAnswersListFromStorage();
+    sendResponse({'answers': answers});
+  }
+});
--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,6 @@
 (function() {
   "use strict";
+  var answers;
 
   // Thanks https://github.com/thieman/github-selfies/blob/master/chrome/selfie.js
   var allowedPaths = [
@@ -13,55 +14,10 @@
     /github.com\/[\w\-]+\/[\w\-]+\/pull\/\d+/
   ];
 
-  var defaultAnswers = [
-    {
-      name: 'Issue: thanks! help fix?',
-      description: "Thanks a lot for filing this issue! Would you like to write a patch for this? We'd be more than happy to walk you through the steps involved."
-    },
-    {
-      name: 'Issue: thanks! looking!',
-      description: "Thanks a lot for filing this issue! We'll triage and take a look at it as soon as possible!"
-    },
-    {
-      name: 'Issue: looks inactive',
-      description: "This issue is fairly old and there hasn't been much activity on it. Closing, but please re-open if it still occurs."
-    },
-    {
-      name: 'Issue: closing, no repro steps',
-      description: "This issue has no reproducible steps. Please re-open this issue if it still occurs, with a JSBin containing a set of reproducible steps. Check this element's CONTRIBUTING.md for an example."
-    },
-    {
-      name: 'Issue: provide repro steps',
-      description: "Please provide a JSBin containing a set of reproducible steps. Check this element's CONTRIBUTING.md for an example."
-    },
-    {
-      name: 'Issue: cannot reproduce',
-      description: "Not reproducible in the latest release. Please re-open this issue if it still occurs, with a JSBin containing a set of reproducible steps. Check this element's CONTRIBUTING.md for an example."
-    },
-    {
-      name: 'PR: thanks! looking!',
-      description: "Thanks for your contribution! We'll triage and take a look at it as soon as possible!"
-    },
-    {
-      name: 'PR: against MD spec',
-      description: "Thanks for your thoughtful contribution! Unfortunately, this is against the Material Design spec, and we can't accept it at this time."
-    },
-    {
-      name: 'PR: needs test',
-      description: "Please add a test case that tests the problem this PR is fixing"
-    }
-  ];
-
-  debugger
-  var localStorageKey = '__GH_CANNED_ANSWERS__EXT__';
-  var saved = localStorage.getItem(localStorageKey);
-  var answers;
-  if (!saved || saved === '') {
-    localStorage.setItem(localStorageKey, JSON.stringify(defaultAnswers));
-    answers = defaultAnswers;
-  } else {
-    answers = JSON.parse(localStorage.getItem(localStorageKey));
-  }
+  chrome.runtime.sendMessage('load', function(response) {
+    answers = response.answers;
+    addAnswerButton();
+  });
 
   function any(array, predicate) {
     for (var i = 0; i < array.length; i++) {
@@ -71,8 +27,6 @@
     }
     return false;
   }
-
-  addAnswerButton();
 
   function addAnswerButton() {
     if (!any(allowedPaths, (path) => path.test(window.location.href))) {
